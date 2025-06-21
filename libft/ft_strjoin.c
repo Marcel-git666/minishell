ca36b2164a72d4e58bb85985bb773b8066d73448
Marcel Mravec <marcel.mravec@gmail.com>
2025-06-21 23:56:59 +0200

    fix libft for norminette errors

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: marcel <marcel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 10:12:46 by mmravec           #+#    #+#             */
-/*   Updated: 2025/06/14 21:35:02 by marcel           ###   ########.fr       */
+/*   Updated: 2025/06/21 23:51:01 by marcel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
-	size_t	len1, len2;
+	size_t	len1;
+	size_t	len2;
 
 	if (!s1 || !s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
-    len2 = ft_strlen(s2);
-	
+	len2 = ft_strlen(s2);
 	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-    if (!result)
-        return (NULL);
+	if (!result)
+		return (NULL);
 	ft_strlcpy(result, s1, len1 + 1);
 	ft_strlcpy(result + len1, s2, len2 + 1);
 	return (result);
