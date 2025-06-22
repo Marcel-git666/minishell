d71e83f0f73ae61e6a4e8fa772ce04d82fbfdfd6
Marcel Mravec <marcel.mravec@gmail.com>
2025-06-22 23:04:15 +0200

    refactor shell_loop

--- src/signals.c
+++ src/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmravec <mmravec@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marcel <marcel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 19:59:08 by mmravec           #+#    #+#             */
-/*   Updated: 2025/02/18 18:03:28 by mmravec          ###   ########.fr       */
+/*   Updated: 2025/06/22 13:12:51 by marcel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	signal_handler(int signum)
 	if (signum == SIGINT)
 	{
 		g_signal_received = signum;
-		write(1, "\n", 1);  // Print newline and prompt directly
+		write(1, "\n", 1); // Print newline and prompt directly
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
@@ -35,7 +35,6 @@ void	setup_signals(void)
 	sa.sa_handler = signal_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-
 	sigaction(SIGINT, &sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
 }
