8f0dee1c207261553814e4828f572640be369bde
Marcel Mravec <mmravec@student.42prague.com>
2025-07-30 10:57:29 +0200

    fix cd -

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcel <marcel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mmravec <mmravec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 19:51:13 by marcel            #+#    #+#             */
-/*   Updated: 2025/07/20 12:14:36 by marcel           ###   ########.fr       */
+/*   Updated: 2025/07/30 10:54:56 by mmravec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static int	handle_oldpwd(t_ast_node *root, t_shell *shell, char *cwd)
 			free(cwd);
 			return (1);
 		}
-		env_set(&shell->env, "OLDPWD", cwd);
+		printf("%s\n", oldpwd);
 		if (chdir(oldpwd) == -1)
 		{
 			perror("cd");
@@ -42,7 +42,8 @@ static int	handle_oldpwd(t_ast_node *root, t_shell *shell, char *cwd)
 			free(cwd);
 			return (1);
 		}
-		return (1);
+		env_set(&shell->env, "OLDPWD", cwd);
+		return (shell->last_exit_code = 0, free(cwd), 1);
 	}
 	return (0);
 }
@@ -74,17 +75,8 @@ static void	update_pwd(t_shell *shell)
  */
 static void	handle_cd_logic(t_ast_node *root, t_shell *shell, char *cwd)
 {
-	if (root->u_content.cmd.arg_count
-		&& ft_strncmp(root->u_content.cmd.args[0], "..", 2) == 0)
-	{
-		env_set(&shell->env, "OLDPWD", cwd);
-		previous_rep(shell, cwd);
-	}
-	else
-	{
-		env_set(&shell->env, "OLDPWD", cwd);
-		path(root, cwd, shell);
-	}
+	env_set(&shell->env, "OLDPWD", cwd);
+	path(root, cwd, shell);
 }
 
 /*
@@ -131,10 +123,7 @@ void	builtin_cd(t_ast_node *root, t_shell *shell)
 		return ;
 	}
 	if (handle_oldpwd(root, shell, cwd))
-	{
-		update_pwd(shell);
 		return ;
-	}
 	handle_cd_logic(root, shell, cwd);
 	free(cwd);
 	update_pwd(shell);
